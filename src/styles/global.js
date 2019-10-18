4e07848ac76f3e9866dee3ce4a8820976c90d7a7
Thiago Marinho <tgmarinho@gmail.com>
2019-10-18 14:30:40 -0400

    MeetApp Web - global styles

---
+++ src/styles/global.js
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  outline: 0;
+  box-sizing: border-box;
+}
+*:focus {
+  outline: 0;
+}
+html, body, #root {
+  height: 100%;
+  body {
+    -webkit-font-smoothing: antialiased;
+  }
+  body, input, button {
+    font: 14px 'Helvetica', sans-serif;
+  }
+  a {
+    text-decoration: none;
+  }
+  ul {
+    list-style: none;
+  }
+  button {
+    cursor: pointer;
+  }
+}
+`;
