0212f7793f6231e28447623eaf7ef654d2ae0f22
Thiago Marinho <tgmarinho@gmail.com>
2019-10-18 13:59:29 -0400

    MeetApp Web - Private Routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
-
+import { Switch } from "react-router-dom";
+import Route from "./Route";
 import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
 import Dashboard from "../pages/Dashboard";
@@ -14,11 +14,11 @@ export default function Routes() {
       <Route path="/" exact component={SignIn} />
       <Route path="/register" component={SignUp} />
 
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/profile" component={Profile} />
-      <Route path="/details/:id" component={Details} />
-      <Route path="/meetup/:id" component={Meetup} />
-      <Route path="/meetup/new" component={Meetup} />
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/profile" component={Profile} isPrivate />
+      <Route path="/details/:id" component={Details} isPrivate />
+      <Route path="/meetup/:id" component={Meetup} isPrivate />
+      <Route path="/meetup/new" component={Meetup} isPrivate />
     </Switch>
   );
 }
