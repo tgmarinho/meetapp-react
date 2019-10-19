c4203095b4bc2dbc90c2baaa9be7709e671b088b
Thiago Marinho <tgmarinho@gmail.com>
2019-10-18 22:47:43 -0400

    MeetApp Web - Displaying Loading on SignIn

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -2,21 +2,24 @@ import { takeLatest, call, put, all } from 'redux-saga/effects';
 
 import api from '~/services/api';
 import history from '~/services/history';
-import { signInSuccess } from './actions';
+import { signInSuccess, signFailure } from './actions';
 
 export function* signIn({ payload }) {
-  const { email, password } = payload;
+  try {
+    const { email, password } = payload;
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
 
-  const response = yield call(api.post, 'sessions', {
-    email,
-    password,
-  });
+    const { token, user } = response.data;
 
-  const { token, user } = response.data;
+    yield put(signInSuccess(token, user));
 
-  yield put(signInSuccess(token, user));
-
-  history.push('/dashboard');
+    history.push('/dashboard');
+  } catch (err) {
+    yield put(signFailure());
+  }
 }
 
 export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
--- src/store/persistReducers.js
+++ src/store/persistReducers.js
@@ -4,7 +4,7 @@ import { persistReducer } from 'redux-persist';
 export default reducers => {
   const persistedReducer = persistReducer(
     {
-      key: 'gobarber',
+      key: 'meetapp',
       storage,
       whitelist: ['auth', 'user'],
     },
